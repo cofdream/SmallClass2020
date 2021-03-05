73ccfe23cf83f899b8382c189830cefb6eba5ecf
cofdream <cofdream@sina.com>
2021-03-05 16:49:43 +0800

    unity shader learn init

---
+++ Assets/Test/Editor/TestWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace NullNamespace
+{
+    public class TestWindow : EditorWindow
+    {
+        [MenuItem("Test/Open")]
+        static void Open()
+        {
+            GetWindow<TestWindow>().Show();
+        }
+
+
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Title");
+                GUILayout.FlexibleSpace();
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/builtin_shaders-2019.4.16f1/Example/CubeShader.shader
@@ -0,0 +1,13 @@
+﻿Shader "Example/cubeShader"
+{
+	Properties
+	{
+		_MainTex("Texture",2D) = "white" {}
+	}
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		UsePass "Example/RedColorShader/REDCOLOR"
+	}
+}
